52c6a6d0e411f02ba7d7f9b5fc90d930ae883ff5
Frederico <fredericogandini@gmail.com>
2026-04-09 15:11:31 -0300

    Questão 3 - Função remover dado

--- funcoes.py
+++ funcoes.py
@@ -4,11 +4,28 @@ Thiago Henrique e Frederico Gandini
 
 Número de horas desperdiçadas: 0.5
 '''
+
+# imports
 import random as rd 
+
+# função rolar dados
 def rolar_dados(n):
     listadados=list()
     for i in range(n):
         listadados.append(rd.randint(1, 6))
     return listadados
 
-
+# função guardar dado
+def guardar_dado(lst_dados_rolados, lst_dados_guardados, i_dado_para_guardar):
+    # guarda o dado da lista no index recebido
+    dado_a_guardar = lst_dados_rolados[i_dado_para_guardar]
+    lst_dados_guardados.append(dado_a_guardar)
+    lst_dados_rolados.pop(i_dado_para_guardar)
+    lst_dados_rolados_guardados = [lst_dados_rolados, lst_dados_guardados]
+    return lst_dados_rolados_guardados
+# função de remover dado
+def remover_dado(rolados, guardados, i):
+    rolados.append(guardados[i])
+    guardados.pop(i)
+    ret=[rolados, guardados]
+    return ret
